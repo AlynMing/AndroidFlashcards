ef99757578fc09aeb4b0f7c854f98b7210662144
OverCry <54381704+OverCry@users.noreply.github.com>
2020-02-15 13:37:11 -0800

    added visibility toggle

--- app/src/main/java/com/example/wongy/flashcard/MainActivity.java
+++ app/src/main/java/com/example/wongy/flashcard/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.wongy.flashcard;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Boolean _visible = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,6 +49,28 @@ public class MainActivity extends AppCompatActivity {
             findViewById(R.id.flashcard_answer1).setBackgroundColor(getResources().getColor(R.color.colorCream));
             findViewById(R.id.flashcard_answer2).setBackgroundColor(getResources().getColor(R.color.colorCream));
             findViewById(R.id.flashcard_answer3).setBackgroundColor(getResources().getColor(R.color.colorLightGreen));
+        }
+    });
+
+    findViewById(R.id.toggle_choice_visibility).setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            if (_visible==true) {
+                _visible=false;
+                findViewById(R.id.flashcard_answer1).setVisibility(view.INVISIBLE);
+                findViewById(R.id.flashcard_answer2).setVisibility(view.INVISIBLE);
+                findViewById(R.id.flashcard_answer3).setVisibility(view.INVISIBLE);
+                ((ImageView) findViewById(R.id.toggle_choice_visibility)).setImageResource(R.drawable.ic_eyec);
+            } else {
+                _visible=true;
+                findViewById(R.id.flashcard_answer1).setVisibility(view.VISIBLE);
+                findViewById(R.id.flashcard_answer2).setVisibility(view.VISIBLE);
+                findViewById(R.id.flashcard_answer3).setVisibility(view.VISIBLE);
+                findViewById(R.id.flashcard_answer1).setBackgroundColor(getResources().getColor(R.color.colorCream));
+                findViewById(R.id.flashcard_answer2).setBackgroundColor(getResources().getColor(R.color.colorCream));
+                findViewById(R.id.flashcard_answer3).setBackgroundColor(getResources().getColor(R.color.colorCream));
+                ((ImageView) findViewById(R.id.toggle_choice_visibility)).setImageResource(R.drawable.ic_eyeo);
+            }
         }
     });
 
